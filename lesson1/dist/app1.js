4c0c6a858473d11240f387e9b5319586bcc62d5e
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-06-28 00:24:49 -0400

    modified app file and config file

--- lesson1/dist/app1.js
+++ lesson1/dist/app1.js
@@ -1,2 +1,6 @@
 "use strict";
 console.log("automatic compiler with tsc   and tsc -w !!!!!");
+var button = document.querySelector("button");
+button.addEventListener("click", function () {
+    console.log("you clicked me!!");
+});
--- lesson1/src/app1.ts
+++ lesson1/src/app1.ts
@@ -1,1 +1,7 @@
 console.log("automatic compiler with tsc   and tsc -w !!!!!");
+
+const button = document.querySelector("button")!;
+
+button.addEventListener("click", () => {
+  console.log("you clicked me!!");
+});
